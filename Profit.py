c6cdc0bdc871ca5ae95f1b27906ac8c236d591ee
Michael Berg <urberg@yahoo.com>
2020-11-02 11:24:44 -0700

    more work in profit

--- Profit.py
+++ Profit.py
@@ -1,11 +1,24 @@
 
 class Profit:
 
-    _SEGMENT = {'Trad':0, 'Low':1, 'High':2, 'Perf':3, 'Size':4}
-
-
-    def __init__(self, segmentSize, growth):
+    def __init__(self, segmentSize, growth, laborCost):
         self.segmentSize = segmentSize * (1 + growth)
         self.survaySum = 0
+        self.labor = laborCost
+        self.market = laborCost
     
-    def 
+    def getMaterialCost(mtbf, baseCost):
+        return baseCost + (mtbf/1000)*0.30
+
+    def effectiveAuto(self, autoLevel, autoLevelLow):
+        return autoLevel*0.5 + autoLevelLow*0.5
+
+    def addSurvay(self, survay):
+        self.survaySum = self.survaySum +  survay
+
+    def setMarket(self, size, growth):
+        self.market = size * (1+growth)
+
+    def getMarketSize(self,survay):
+        return survay/self.survaySum * self.market
+
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
 import dataGathering as dg
 import marketing as mk
 from model import Model
 import pandas as pd
+from constants import Constants as con
+import pulp
 
-df = dg.readInSimData()
 
-x = df[['Material', 'Size', 'MTBFABS']]
+MTBFM = LpVariable("mtbf", con.mtbf('Trad'), con.mtbf('Trad') + 5000)
 
-print(x)
